fe9082fc7875425a8f60da8e33cb97291cf337a9
riya-eng01 <riyasingheng01@gmail.com>
2026-06-04 13:18:36 +0530

    Deleted Uneccerssary files

--- Flowcharts.java
+++
@@ -1,9 +0,0 @@
-public class Flowcharts{
-    public static void main(String args[]) {
-        // loop
-        for(int i = 10; i <= 98; i+= 2 ){
-
-            System.out.print(i + " ");
-        }
-    }
-}
