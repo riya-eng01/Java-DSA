625977e034ad6626460dfedc75ed801734b60f87
riya-eng01 <riyasingheng01@gmail.com>
2026-06-04 13:09:21 +0530

    localrepo

---
+++ Flowcharts.java
@@ -0,0 +1,9 @@
+public class Flowcharts{
+    public static void main(String args[]) {
+        // loop
+        for(int i = 10; i <= 98; i+= 2 ){
+
+            System.out.print(i + " ");
+        }
+    }
+}
