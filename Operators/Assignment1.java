44092a97d6f628b291f32c88e501de508a1eb109
riya-eng01 <riyasingheng01@gmail.com>
2026-06-05 08:40:20 +0530

    Assignment

---
+++ Operators/Assignment1.java
@@ -0,0 +1,12 @@
+public class Assignment1 {
+    public static void main(String args[]) {
+        // Question-1
+        int x = 2, y = 5;
+
+        int exp1 = (x * y / x); // 5
+        int exp2 = (x * (y / x)); // 4
+
+        System.out.println(exp1);
+        System.out.println(exp2);
+    }
+}
