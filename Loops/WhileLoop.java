c97333754af3727955d2ff490c20ebb271c95085
riya-eng01 <riyasingheng01@gmail.com>
2026-06-07 07:38:28 +0530

    While Loop

---
+++ Loops/WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String args[]) {
+
+        // Printing "Hello world" 100 times
+        int i = 1;
+        while(i <= 100) {
+            System.out.println(i + " Hello world");
+            i++; 
+        }
+    }
+}
