7abf49477ef003fedef939da9d070de07c9b61ff
Riya Singh <riyasingheng01@gmail.com>
2026-06-10 15:17:07 +0530

    Add java files

--- Function & Methods/CheckPrime.java
+++ Function & Methods/CheckPrime.java
@@ -1,24 +1,23 @@
 public class CheckPrime {
     // function to check if a number is prime or not
     public static boolean isPrime(int n) {
-        boolean isPrime = true;
 
-        if (n == 0) {
-            isPrime = true;
-        } else {
-            for (int i = 2 ; i <= Math.sqrt(n); i++){
-                if (n % i == 0) {
-                    isPrime = false;
-                }
-            }
+        if (n == 2) {
+            return true;
         }
         
-        return isPrime;
+        for (int i = 2 ; i <= Math.sqrt(n); i++){
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
+    // Main function
     public static void main(String args[]) {
         int n = 4; 
-        System.out.println(isPrime(n));
+        System.out.println(isPrime(3));
 
     }
 }
