a41e5d639ae0f5a50888b50d385dd4c29a247e7b
Riya Singh <riyasingheng01@gmail.com>
2026-06-13 23:14:07 +0530

    Add java files

--- Array (Part - I)/InputOutputArray.java
+++ Array (Part - I)/InputOutputArray.java
@@ -6,13 +6,33 @@ public class InputOutputArray {
         // taking input in an array
         int marks[] = new int[50];
 
-        System.out.print("Enter phy marks: ");
-        marks[0] = sc.nextInt();
-        System.out.print("Enter chem marks: ");
-        marks[1] = sc.nextInt();
-        System.out.print("Enter math marks: ");
-        marks[2] = sc.nextInt();
+        // System.out.print("Enter phy marks: ");
+        // marks[0] = sc.nextInt();
+        // System.out.print("Enter chem marks: ");
+        // marks[1] = sc.nextInt();
+        // System.out.print("Enter math marks: ");
+        // marks[2] = sc.nextInt();
 
-        System.out.print("Maths marks = " + marks[2]);
+        marks[0] = 75; // phy
+        marks[1] = 80; // chem
+        marks[2] = 90; // maths
+        
+        System.out.println("Math marks = " + marks[2]);
+
+        // Updating array marks
+        marks[2] = 85;
+        System.out.println("Math marks = " + marks[2]);
+
+        // adding in array
+        marks[2] = marks[2] + 1;
+        System.out.println(marks[2]);
+
+        // Calculating average
+        int avg = (marks[0] + marks[1] + marks[2]) / 3;
+        System.out.println("Average = " + avg);
+
+        // length of an array
+        System.out.println("length = " + (marks.length))
+
     }
 }
