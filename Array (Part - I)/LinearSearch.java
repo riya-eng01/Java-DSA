ed3c21b446a7159db640e7c60562a3cb613f1273
riya-eng01 <riyasingheng01@gmail.com>
2026-06-14 08:46:52 +0530

    Add arrays file

---
+++ Array (Part - I)/LinearSearch.java
@@ -0,0 +1,23 @@
+public class LinearSearch {
+    // function for linear Serach
+    public static int linearSearch(int arr[], int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i; // return index
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
+        int key = 10;
+
+        int index = linearSearch(numbers, key);
+        if (index == -1) {
+            System.out.println("Key not Found!");
+        } else {
+            System.out.println("Key is at index : " + index);
+        }
+    }
+}
