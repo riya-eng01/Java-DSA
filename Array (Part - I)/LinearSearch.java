4a31d13b6d0ee2c5a1aa1e41960f3396ff59390d
riya-eng01 <riyasingheng01@gmail.com>
2026-06-14 09:00:07 +0530

    modified

--- Array (Part - I)/LinearSearch.java
+++ Array (Part - I)/LinearSearch.java
@@ -8,10 +8,22 @@ public class LinearSearch {
         }
         return -1;
     }
+    // function for 'String' Linear Search
+    public static int linearSearch(String arr[], String key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    } 
 
     public static void main(String args[]) {
         int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
         int key = 10;
+
+        String menu[] = {"Dosa", "Samosa", "Chole Bhature", "Frooti"};
+        String item = "Samosa";
 
         int index = linearSearch(numbers, key);
         if (index == -1) {
