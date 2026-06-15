cbcebafd94d516dc584e32c6ec56426a36fab795
riya-eng01 <riyasingheng01@gmail.com>
2026-06-15 08:48:32 +0530

    Add java files

---
+++ Array (Part - I)/PairsInArray.java
@@ -0,0 +1,16 @@
+public class PairsInArray {
+    // function to print pairs In Array
+    public static void printPairs(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                System.out.print("(" + arr[i] + "," + arr[j] + ")");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = {2, 4, 6, 8, 10};
+        printPairs(numbers);
+    } 
+}
