7411e9aec5975453a298ef36d261863a10f2a818
Riya Singh <riyasingheng01@gmail.com>
2026-06-17 23:51:16 +0530

    Add marks

--- Array (Part - I)/ArrayFunctionArgument.java
+++ Array (Part - I)/ArrayFunctionArgument.java
@@ -7,15 +7,13 @@ public class ArrayFunctionArgument {
     }
 
     public static void main(String args[]) {
-        int marks[] = {97, 98, 99};
+        int marks[] = {97, 98, 99, 100};
         update(marks);
 
         //print an array
         for (int i = 0; i < marks.length; i++) {
             System.out.print(marks[i] + " ");
         }
-
-        
         System.out.println();
     }
 }
