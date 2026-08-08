b3366f25830ea20dfcfa2cdd3cd8b8d8b0155a45
Riya Singh <riyasingheng01@gmail.com>
2026-08-08 11:30:00 +0530

    Practice code

--- Practice Folder/Main.java
+++ Practice Folder/Main.java
@@ -13,7 +13,39 @@ public class Main{
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         // Print square pattern
-        printSquare(10);        
+        printSquare(4); 
+        
+        // Ques -1
+        // System.out.print("Enter a number: ");
+        // int num1 = sc.nextInt();
+        // System.out.print("Enter second number: ");
+        // int num2 = sc.nextInt();
+        // System.out.print("Enter a third number: ");
+        // int num3 = sc.nextInt();
 
+        // int avg = (num1 + num2 + num3 )/ 3;
+        // System.out.println("Average = "+ avg);
+
+        // Ques-2
+        // System.out.print("Enter side of a square: ");
+        // float side = sc.nextFloat();
+        // float area = side * side;
+        // System.out.println("Area of square = " + area);
+
+        // Ques-3
+    //     System.out.print("Enter pencil price: ");
+    //     float pencilPrice = sc.nextFloat();
+    //     System.out.print("Enter pen price: ");
+    //     float penPrice = sc.nextFloat();
+    //     System.out.print("Enter earaser price: ");
+    //     float earaserPrice = sc.nextFloat();
+
+    //     float total_bill = pencilPrice + penPrice + earaserPrice;
+    //     System.out.println("Total Bill: " + total_bill);
+    // 
+
+    int $ = 24;
+    System.out.println($);
+    
     }
 }
